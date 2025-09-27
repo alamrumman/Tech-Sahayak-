cadb7f5a548ecf43514cadda05f4213dabc62f4a
Rumi-stack <alamrumman039@gmail.com>
2025-09-28 02:26:42 +0530

    resized comp

--- Front/src/Madecomp/Hero.jsx
+++ Front/src/Madecomp/Hero.jsx
@@ -37,14 +37,14 @@ function Hero() {
           {/* We add a space and specific styling to make it look like one sentence */}
         </div>
 
-        <div className="flex flex-col md:flex-row md:justify-center md:items-end gap-8 sm:gap-12 md:gap-16 lg:gap-20 text-black mt-12 sm:mt-16 md:mt-20">
+        <div className="flex flex-col md:flex-row md:justify-center md:items-end gap-8 sm:gap-12 md:gap-20 lg:gap-50 text-black mt-12 sm:mt-16 md:mt-10">
           <div className="flex justify-center md:justify-end w-full md:w-auto">
-            <div className="scale-90 sm:scale-100 md:scale-100">
+            <div className="scale-80 sm:scale-100 md:scale-120 ">
               <Cardcomp />
             </div>
           </div>
           <div className="flex justify-center w-full md:w-auto">
-            <div className="scale-90 sm:scale-100 md:scale-100">
+            <div className="scale-80 sm:scale-100 md:scale-120">
               <SHCupload />
             </div>
           </div>
