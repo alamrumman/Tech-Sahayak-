42e68c2a2ef18ce8fa1f34b64e2959594a3524a4
Rumi-stack <alamrumman039@gmail.com>
2025-10-13 18:58:19 +0530

    Structured backend for login and signup of team mates

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,9 +1,38 @@
 const mongoose = require("mongoose");
-
+const bcrypt = require("bcryptjs");
 const userSchema = new mongoose.Schema({
-  name: String,
-  age: Number,
-  district: String,
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true, // No two users can have the same email
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  Role: {
+    type: String,
+    required: true,
+    
+  },
+});
+
+//middleware for hashing the password before the user is created.
+userSchema.pre("save", async function (next) {
+  // Only hash the password if it has been modified (or is new)
+  if (!this.isModified("password")) {
+    return next();
+  }
+  // Hash the password with a cost factor of 12
+  const salt = await bcrypt.genSalt(12);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
 });
 
 module.exports = mongoose.model("User", userSchema);
--- backend/server.js
+++ backend/server.js
@@ -15,10 +15,9 @@ mongoose
   .then(() => console.log("MongoDB Connected..."))
   .catch((err) => console.error(err));
 
-
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: [process.env.FRONTEND_URL, "http://localhost:5173"],
   })
 );
 
